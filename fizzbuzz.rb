b9eb69367d026534650de426f0c5e53b41b0b3ff
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-24 13:50:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,12 +8,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
   "FizzBuzz"
+elsif int % 3 == 0
+  "Fizz"
 elsif int % 5 == 0
   "Buzz"
-elsif int % 3 == 0 && int % 5 == 0
-  "FizzBuzz"
 else
-   nil
+   int
   end
 end
 
